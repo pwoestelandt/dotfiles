e32b035ce631188c5b5819c89ad618def865156f
Paul Woestelandt <paul.woestelandt@inthememory.com>
2024-02-16 17:57:25 +0100

    feat(nvim): add basic keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local Util = require("lazyvim.util")
+local map = Util.safe_keymap_set
+
+map("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+map("n", "<leader>pv", vim.cmd.Ex, { desc = "enter Ex mode" })
+map("n", "<leader>w", vim.cmd.write, { desc = "save file" })
