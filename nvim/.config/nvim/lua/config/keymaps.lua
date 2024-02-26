71cac3e63d6e34963f56f74ba362f14895ea5cc4
Paul Woestelandt <paul.woestelandt@inthememory.com>
2024-02-26 09:56:54 +0100

    feat(nvim) update telescope config

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,5 +4,3 @@ local map = Util.safe_keymap_set
 map("i", "jk", "<Esc>", { desc = "Exit insert mode" })
 map("n", "<leader>pv", vim.cmd.Ex, { desc = "enter Ex mode" })
 map("n", "<leader>w", vim.cmd.write, { desc = "save file" })
-
-map("n", "<leader>fg", require("telescope.builtin").live_grep, { desc = "Live grep" })
