530ebca167b39c873e1e3a03644bfbffc6e342c9
WOESTELANDT Paul <paul.woestelandt@inthememory.com>
2025-01-31 15:08:21 +0100

    feat: boostrap bare nvim with lazy plugin manager

--- nvim/.config/nvim/lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
-local Util = require("lazyvim.util")
-local map = Util.safe_keymap_set
-
-map("i", "jk", "<Esc>", { noremap = true, silent = true, desc = "Exit insert mode" })
-map("n", "<leader>pv", vim.cmd.Ex, { noremap = true, silent = true, desc = "enter Ex mode" })
