a98871cd3038595e8f83b6afdb5938c2507cf81f
WOESTELANDT Paul <paul.woestelandt@inthememory.com>
2025-03-03 21:18:49 +0100

    fix: autoformat

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -1,19 +1,30 @@
 return {
-	"stevearc/conform.nvim",
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		{ "williamboman/mason.nvim", opts = {} },
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		local conform = require("conform")
+		local ensure_installed = { "stylua" }
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-		conform.setup({
-			formatters_by_ft = {
-				lua = { "stylua" }, -- brew install stylua
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
 			},
+		})
 
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			},
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+		local format_augroup = vim.api.nvim_create_augroup("format", { clear = true })
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = format_augroup,
+			callback = function()
+				vim.lsp.buf.format()
+			end,
 		})
 	end,
 }
